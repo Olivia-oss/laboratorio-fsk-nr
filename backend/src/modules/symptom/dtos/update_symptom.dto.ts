a4cbcac1fa5e51b67a517664b12c05cbf485e60e
olivia <11.oly.adame@gmail.com>
2025-01-17 19:07:22 -0600

    CRUD functionality for symptom

--- backend/src/modules/symptom/dtos/update_symptom.dto.ts
+++ backend/src/modules/symptom/dtos/update_symptom.dto.ts
@@ -1,10 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsOptional, IsString } from 'class-validator';
 
 export class UpdateSymptomDto {
+  @ApiProperty()
   @IsString()
   @IsOptional()
   name: string;
 
+  @ApiProperty()
   @IsString()
   @IsOptional()
   description: string;
